32b62cd91dec0ddf85ded96ac59d0348ebe03f6c
codemaoz90 <codemaoz@gmail.com>
2021-02-18 15:11:35 +0100

    J-Functions Done

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,11 @@
 function halve(number) {
-  // complete the function here
+	// complete the function here
+	return number / 2;
 }
 
 var result = halve(12);
-
+console.log(result);
+var result = halve(24);
+console.log(result);
+var result = halve(36);
 console.log(result);
